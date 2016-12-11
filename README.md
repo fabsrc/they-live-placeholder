59adce59bac2efe828230faa4560512b47c27648
Fabian Schneider <fabbbbbi+git@googlemail.com>
2016-12-11 17:05:32 +0100

    :sunglasses:

---
+++ README.md
@@ -0,0 +1,63 @@
+# They Live Placeholder
+
+> :sunglasses:
+
+Generates placeholder images in the style of billboards from John Carpenter's movie [*They Live*](https://www.youtube.com/watch?v=JI8AMRbqY6w).
+
+
+## Install
+
+```bash
+$ composer install
+```
+
+## Start
+
+```bash
+$ php -S 0.0.0.0:3000 index.php
+```
+
+## Usage
+
+```
+http://localhost:3000/{width}x{height}.{format}?text={text}
+```
+
+* `width`: Width of the image.
+* `height` *(optional)*: If omitted, a squared image will be created based on the specified width.
+* `format` *(optional)*: `png` (default), `gif` or `jpg` are valid formats.
+* `text` *(optional)*: If no text is specified, random slogans from the movie are used.
+
+
+### Examples
+
+* http://localhost:3000/300
+* http://localhost:3000/300x600
+* http://localhost:3000/900x1200.png
+* http://localhost:3000/900x1200.jpg
+* http://localhost:3000/900x1200.jpeg
+* http://localhost:3000/500.jpg
+* http://localhost:3000/500.jpg?text=Hello%20World
+* http://localhost:3000/500x700.jpg?text=Hello%20World
+
+
+#### Example Image
+
+> http://localhost:3000/600x400
+
+![They Live Placeholder Example](https://raw.githubusercontent.com/fabsrc/they-live-placeholder/example/600x400.png)
+
+### Use with Apache
+
+In order to use this script with an *Apache* server use a `.htaccess` file to redirect all requests to `index.php`.
+
+```
+RewriteEngine on
+RewriteCond %{REQUEST_FILENAME} !-d
+RewriteCond %{REQUEST_FILENAME} !-f
+RewriteRule . index.php [L]
+```
+
+## License
+
+Licensed under the [MIT License](http://opensource.org/licenses/mit-license.php).
---
+++ index.php
@@ -0,0 +1,85 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+
+use GDText\Box;
+use GDText\Color;
+
+$url = parse_url($_SERVER['REQUEST_URI']);
+preg_match('/\/(\d+)(x(\d+))?(\.(\w+))?/', $url['path'], $match);
+$width = !empty($match[1]) ? $match[1] : 500;
+$height = !empty($match[3]) ? $match[3] : $width;
+$width = min($width, 3000);
+$height = min($height, 3000);
+
+$format = isset($match[5]) ? $match[5] : 'png';
+
+$slogans = [
+'Obey',
+'Obey and Conform',
+'Marry and Reproduce',
+'Watch T.V.',
+'Stay Asleep',
+'No Thought',
+'Submit',
+'Conform',
+'This is your God',
+'No Independent Thought',
+'Buy',
+'Do Not Think',
+'Do Not Question Authority',
+'Work 8 Hours',
+'Play 8 Hours',
+'Sleep 8 Hours',
+'Stay Asleep'
+];
+
+$random_slogan = $slogans[mt_rand(0, count($slogans) - 1)];
+
+if (isset($_SERVER['QUERY_STRING'])) {
+    parse_str($_SERVER['QUERY_STRING'], $queries);
+    $text = strtoupper(
+        array_key_exists('text', $queries) ? 
+        $queries['text']  : 
+        $random_slogan
+    );
+} else {
+    $text = strtoupper($random_slogan);
+}
+
+$font_size = min([$height / 8.5,$width / 8.5]);
+
+$im = imagecreatetruecolor($width, $height);
+$backgroundColor = imagecolorallocate($im, 255, 255, 255);
+imagefill($im, 0, 0, $backgroundColor);
+
+$box = new Box($im);
+$box->setFontFace(__DIR__.'/fonts/font.otf');
+$box->setFontColor(new Color(0, 0, 0));
+$box->setFontSize($font_size);
+$box->setBox(20, 20, $width - 40, $height - 40);
+$box->setTextAlign('center', 'center');
+$box->setLineHeight(1);
+$box->draw($text);
+
+switch ($format) {
+case 'gif':
+    header('Content-type: image/gif');
+    imagegif($im);
+    break;
+
+case 'jpg':
+case 'jpeg':
+    header('Content-type: image/jpeg');
+    imagejpeg($im);
+    break;
+
+case 'png':
+    header('Content-type: image/png');
+    imagepng($im);
+    break;
+
+default:
+    die('Unsupported File Format. Use png, jpeg or gif.');
+    break;
+}
+?>
